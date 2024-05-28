a83e6df3555295122c0b191c0090869b45df0e8a
MiryamElizabeth2006 <157499620+MiryamElizabeth2006@users.noreply.github.com>
2024-05-28 11:38:19 -0500

    Mi primer commit

---
+++ componentes.js
@@ -0,0 +1,11 @@
+modificarTexto =  function(){
+    let cmpTitulo1;
+    cmpTitulo1 = document.getElementById("texto1");
+    cmpTitulo1.innerText = "TEXTO MODIFICADO"
+}
+
+modificarImagen = function(){
+    let cmpImagen1;
+    cmpImagen1 = document.getElementById("imagen1");
+    cmpImagen1.src="desamor.png"
+}
